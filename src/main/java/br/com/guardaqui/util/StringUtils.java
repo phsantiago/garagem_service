26fb4e6ebee8087c3d457df6e2d04359ead7f832
Kiran1520 <joao.r.soares@outlook.com>
2016-07-26 23:20:51 -0300

    Melhoria de senha

--- src/main/java/br/com/guardaqui/util/StringUtils.java
+++ src/main/java/br/com/guardaqui/util/StringUtils.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class StringUtils {
 	private final static Pattern hasNumber = Pattern.compile("\\d");
-	private final static Pattern hasChar = Pattern.compile("\\w");
+	private final static Pattern hasChar = Pattern.compile("\\[a-z][A-Z]");
 	public static boolean isEmail(String email){
 		return email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 	}
